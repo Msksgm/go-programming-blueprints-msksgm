da843251a879d3abaff26281b42b702b98e80b39
Msksgm <msksgm3124@gmail.com>
2021-12-18 12:22:39 +0900

    add: p.46

--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -25,6 +32,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します
+	// Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("セキュリティキー")
+	fmt.Println(os.Getenv("CLIENT_ID"))
+	gomniauth.WithProviders(
+		facebook.New(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"), "http://localhost:8080/auth/callback/facebook"),
+		github.New(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"), "http://localhost:8080/auth/callback/github"),
+		google.New(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"), "http://localhost:8080/auth/callback/google"),
+	)
+
 	r := newRoom()
 	// ルート
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
